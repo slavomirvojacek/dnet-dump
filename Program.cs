cb9374a40d276ca0de8cf7ecf526e223659e722f
Slavomir Vojacek <slavomirvojacek@gmail.com>
2016-01-15 14:23:37 +0000

    classes with constructors, linq, and more

--- Program.cs
+++ Program.cs
@@ -5,18 +5,42 @@ using System.Threading.Tasks;
 
 namespace main
 {
+    public class Customer
+    {
+        public string FirstName;
+        public string LastName;
+        public string City;
+
+        public Customer(string firstname, string lastname, string city)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.City = city;
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
 
+            Console.WriteLine("");
+
             foreach (string k in GetUniqueValues())
             {
                 Console.WriteLine("Value: {0}", k);
             }
 
-            Console.Read();
+            Console.WriteLine("");
+
+            Ints();
+
+            Console.WriteLine("\n");
+
+            SelectCustomerData();
+
+            // Console.Read();
         }
 
         private static HashSet<string> GetUniqueValues()
@@ -25,10 +49,44 @@ namespace main
             {
                 {"a", "Hello"}, {"b", "World"}, {"c", "Hello"}, {"d", "World"}
             };
-            
+
             List<string> list = new List<string>(obj.Values);
-            
+
             return new HashSet<string>(list);
+        }
+
+        private static void Ints()
+        {
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            var numQuery =
+                from num in numbers
+                where (num % 2) == 0
+                select num;
+
+            foreach (int num in numQuery)
+            {
+                Console.Write("{0,1} ", num);
+            }
+        }
+
+        private static void SelectCustomerData()
+        {
+            Customer[] customers = new Customer[] {
+                new Customer("John", "Doe", "New York"),
+                new Customer("Jason", "Statham", "London"),
+                new Customer("Albus", "Dumbledore", "London")
+            };
+
+            IEnumerable<Customer> customerQuery =
+                from customer in customers
+                where customer.City == "London"
+                select customer;
+
+            foreach (Customer customer in customerQuery)
+            {
+                Console.WriteLine(customer.LastName + ", " + customer.FirstName);
+            }
         }
     }
 }
